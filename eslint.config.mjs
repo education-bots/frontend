298f63009dcefcdf3dd531593e763c7cc002f5f1
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-09-28 08:38:08 +0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,15 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+   {
+    rules: {
+      "react/no-unescaped-entities":"off",
+      "@typescript-eslint/quotes":"off",
+      "quotes":[0],
+       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+       
+  
+    }
+  }
 ];
 
 export default eslintConfig;
