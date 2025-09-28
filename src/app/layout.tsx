ad87fc2775499389acb0bd688ebec8b5c9a642fa
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-09-29 02:51:36 +0500

    route fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
  
  
  
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
