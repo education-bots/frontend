fb0e9e070541ca1191395340940a9a4927248504
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-09-28 08:34:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import { Toaster } from "react-hot-toast";
 
 
